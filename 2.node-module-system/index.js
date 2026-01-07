1df84cf40692e09f062230ea88abcb04cbb7ada2
Madhankannan02 <madhankannan29@gmail.com>
2026-01-07 20:38:00 +0530

    Added the import using try and catch method

--- 2.node-module-system/index.js
+++ 2.node-module-system/index.js
@@ -1,2 +1,9 @@
 const firstModule = require('./first-module');
 console.log(firstModule.multiplication(2,5));
+
+try{
+ let result = firstModule.division(0,0);
+ console.log(result);
+} catch(error){
+ console.log(error.message);
+}
